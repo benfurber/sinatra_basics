219dd7e915b495d8af324f76de328d111e197808
Igor Ryabchuk <riabtchuk@gmail.com>
2018-04-30 14:25:24 +0100

    p the params

--- app.rb
+++ app.rb
@@ -16,7 +16,13 @@ get '/salad' do
   'Lol. No cake.'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
